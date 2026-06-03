68efbd8a28bb5d08682242159b5b558d99623bb6
Jacob Craiglow <jacobcraiglow1@gmail.com>
2026-06-02 21:49:44 -0400

    update neo_tree setting to not automatically start

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set('n', '<leader>hx', mark.add_file, {desc = 'Harpoon mark file'}) 
+vim.keymap.set('n', '<leader>hx', mark.add_file, {desc = 'Harpoon mark file'})
 vim.keymap.set('n', '<leader>hc', ui.toggle_quick_menu, {desc = 'Harpoon open quick menu'})
 vim.keymap.set('n', '<leader>hg', ui.nav_next, {desc = 'Harpoon jump one file'})
 
@@ -39,43 +39,6 @@ vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
 --LSP
 --vim.lsp.enable("basedpyright")
-
---Neo-Tree
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
-require("neo-tree").setup({
-  event_handlers = {
-    {
-      event = "file_open_requested",
-      handler = function()
-        -- auto close
-        vim.cmd("Neotree close")
-        -- OR
-        -- require("neo-tree.command").execute({ action = "close" })
-      end
-    },
-  icon = {
-      folder_closed = "",
-      folder_open = "",
-      folder_empty = "󰜌",
-      provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
-        if node.type == "file" or node.type == "terminal" then
-          local success, web_devicons = pcall(require, "nvim-web-devicons")
-          local name = node.type == "terminal" and "terminal" or node.name
-          if success then
-            local devicon, hl = web_devicons.get_icon(name)
-            icon.text = devicon or icon.text
-            icon.highlight = hl or icon.highlight
-          end
-        end
-      end,
-      -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-      -- then these will never be used.
-      default = "*",
-      highlight = "NeoTreeFileIcon",
-      use_filtered_colors = true, -- Whether to use a different highlight when the file is filtered (hidden, dotfile, etc.).
-    },
-  }
-})
 
 --Markview
 vim.cmd("set nowrap")
--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -7,6 +7,41 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
+    cmd = "Neotree",
+    keys = {
+      { "<leader>e", ":Neotree toggle<CR>", desc = "Toggle Neo-tree" },
+    },
+    config = function()
+      require("neo-tree").setup({
+        event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              -- auto close
+              vim.cmd("Neotree close")
+            end
+          },
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "󰜌",
+          provider = function(icon, node, state)
+            if node.type == "file" or node.type == "terminal" then
+              local success, web_devicons = pcall(require, "nvim-web-devicons")
+              local name = node.type == "terminal" and "terminal" or node.name
+              if success then
+                local devicon, hl = web_devicons.get_icon(name)
+                icon.text = devicon or icon.text
+                icon.highlight = hl or icon.highlight
+              end
+            end
+          end,
+          default = "*",
+          highlight = "NeoTreeFileIcon",
+          use_filtered_colors = true,
+        },
+      })
+    end,
   }
 }
